9b1cc8dc49f92c7a13f8f067938eef7758405473
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-02-22 04:00:51 +0300

    Bubble sort için yorum satırı eklendi

--- push_swap.c
+++ push_swap.c
@@ -12,14 +12,14 @@ void num_swap(int *a, int *b)
 
 int main()
 {
-    int nums[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int nums[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};  // 0 1 2 3
     bool check = true;
     int size = sizeof(nums) / sizeof(nums[0]);
     int k = 0;
-    for (int i = 0; i < size-1; i++)
+    for (int i = 0; i < size; i++)
     {
         check = true;
-        for (int j = 1; j < size-i; j++)
+        for (int j = 1; j < size; j++)
         {
             if (nums[j-1] > nums[j])
             {
@@ -27,7 +27,7 @@ int main()
                 num_swap(&nums[j-1], &nums[j]);
             }
         }
-        if (check)
+        if (check) // if bloğuna hiç girilmemişse demekki dizi artık sıralanmıştır.
             break;
     }
     printf("\n");
