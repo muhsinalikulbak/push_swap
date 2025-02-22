184bb1df12e9be942d26a78dbd265f142a28c627
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-02-22 04:17:46 +0300

    Selection Sort Çalışması

--- push_swap.c
+++ push_swap.c
@@ -12,24 +12,21 @@ void num_swap(int *a, int *b)
 
 int main()
 {
-    int nums[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};  // 0 1 2 3
-    bool check = true;
+    int nums[10] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};  // Selection Sort (Seçmeli Sıralama)
     int size = sizeof(nums) / sizeof(nums[0]);
-    int k = 0;
+    int low_idx = 0;
     for (int i = 0; i < size; i++)
     {
-        check = true;
-        for (int j = 1; j < size; j++)
+        low_idx = i;
+        for (int j = i+1; j < size; j++)
         {
-            if (nums[j-1] > nums[j])
-            {
-                check = false;
-                num_swap(&nums[j-1], &nums[j]);
-            }
+            if (nums[j] < nums[low_idx])
+                low_idx = j;
         }
-        if (check) // if bloğuna hiç girilmemişse demekki dizi artık sıralanmıştır.
-            break;
+        num_swap(&nums[i], &nums[low_idx]);
     }
+
+    int k = 0;
     printf("\n");
     while (k < size)
     {
