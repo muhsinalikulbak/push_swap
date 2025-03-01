46256a074fbcbe24c2fe8046342bce6d6b1cc12d
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-03-01 21:23:18 +0300

    Makefile düzeltildi ve sıralama için her şey hazır

--- Makefile
+++ Makefile
@@ -5,51 +5,34 @@
 #                                                     +:+ +:+         +:+      #
 #    By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/02/25 18:43:16 by mkulbak           #+#    #+#              #
-#    Updated: 2025/02/28 01:37:58 by mkulbak          ###   ########.fr        #
+#    Created: 2025/03/01 21:21:11 by mkulbak           #+#    #+#              #
+#    Updated: 2025/03/01 21:21:22 by mkulbak          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME = push_swap.a
+NAME = push_swap
 
-CC = gcc
+CFLAGS = gcc -Wall -Wextra -Werror
 
-CFLAGS = -Wall -Wextra -Werror
+SOURCES = push_swap.c push_swap_utils.c stack_functions.c stack_utils.c
 
-RM = rm -f
+LIBFT = libft/libft.a
 
-LIBFT_DIR = ./libft
 
-SRCS = push_swap.c stack_utils.c push_swap_utils.c stack_functions.c
 
-LIBFT_OBJS = ./libft/ft_atol.o ./libft/ft_bzero.o ./libft/ft_isalnum.o ./libft/ft_isalpha.o ./libft/ft_isascii.o ./libft/ft_isdigit.o\
-./libft/ft_isprint.o ./libft/ft_memchr.o ./libft/ft_memcmp.o ./libft/ft_striteri.o\
-./libft/ft_memcpy.o ./libft/ft_memmove.o ./libft/ft_memset.o ./libft/ft_putendl_fd.o\
-./libft/ft_putchar_fd.o ./libft/ft_putstr_fd.o ./libft/ft_putnbr_fd.o\
-./libft/ft_strchr.o ./libft/ft_strlcpy.o ./libft/ft_strmapi.o\
-./libft/ft_strdup.o ./libft/ft_split.o ./libft/ft_calloc.o ./libft/ft_itoa.o\
-./libft/ft_strlen.o ./libft/ft_strncmp.o ./libft/ft_substr.o ./libft/ft_strtrim.o\
-./libft/ft_strnstr.o ./libft/ft_strrchr.o ./libft/ft_strjoin.o\
-./libft/ft_tolower.o ./libft/ft_toupper.o ./libft/ft_strlcat.o
+all: $(NAME)
 
-OBJ = ${SRCS:.c=.o}
 
-all: ${NAME}
+$(NAME): $(SOURCES)
+	make -C ./libft -s 
+	$(CFLAGS)  -o $(NAME) $(SOURCES) $(LIBFT)
 
-${NAME}: ${OBJ}
-		make -C ${LIBFT_DIR}
-		ar rcs ${NAME} ${OBJ} ${LIBFT_OBJS}
-		$(CC) $(NAME) -o push_swap
-		$(RM) $(OBJ)
-		
 clean:
-		make clean -C ${LIBFT_DIR}
-		${RM} ${OBJ}
+	make clean -C ./libft -s
 
 fclean: clean
-		make fclean -C ${LIBFT_DIR}
-		${RM} ${NAME}
-		$(RM) push_swap
+	make fclean -C ./libft -s
+	rm -f $(NAME)
 
 re: fclean all
 
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 14:21:41 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/02/25 19:03:19 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/01 20:58:10 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,11 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-
-char	**free_all(char **result);
-char	*ft_itoa(int n);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
-char	**ft_split(char const *s, char c);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strrchr(const char *s, int c);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-char	*ft_strdup(const char	*s1);
-long	ft_atol(const char *str);
-void	*ft_calloc(size_t count, size_t size);
-size_t	ft_strlen(const char *s);
-void	ft_bzero(void *s, size_t n);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
 int		ft_isdigit(int c);
-int		ft_isprint(int c);
-void	*ft_memset(void *s, int c, size_t n);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-void	*ft_memchr(const void *s, int c, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char	*ft_strchr(const char *s, int c);
+char	**free_all(char **result);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char	*s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strtrim(char const *s1, char const *set);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putendl_fd(char *s, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putchar_fd(char c, int fd);
-int		ft_isascii(int c);
+char	**ft_split(char const *s, char c);
+long	ft_atol(const char *str);
 #endif
