a6a476159c8cc7e9c8ea4d3a7d0d23155adb0a12
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-03-05 17:14:25 +0300

    index set fonksiyonu yazıldı

--- binary_radix_sort.c
+++ binary_radix_sort.c
@@ -1,15 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   radix_sort.c                                       :+:      :+:    :+:   */
+/*   binary_radix_sort.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 14:33:57 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/03/05 15:37:10 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/05 17:13:32 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-ra
+void	set_index(t_stack **a)
+{
+	int		i;
+	int		size;
+	int		index;
+	int		prev_min;
+	t_stack	*temp;
+	t_stack	*min_temp;
+
+	i = 0;
+	prev_min = INT_MAX;
+	size = stack_size(*a);
+	while (i < size)
+	{
+		temp = *a;
+		min_temp = temp->content;
+		while (temp != NULL)
+		{
+			if (temp->content != prev_min && temp->content < min_temp->content)
+				min_temp = temp;
+			temp = temp->next;
+		}
+		min_temp->index = i;
+		prev_min = min_temp->content;
+		i++;
+	}
+}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 18:43:24 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/03/02 16:45:01 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/05 17:14:06 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@
 typedef struct s_list
 {
 	int				content;
+	int				index;
 	struct s_list	*next;
 }	t_stack;
 
 t_stack	*new_stack(int num);
 t_stack	*get_last(t_stack *stack);
+void	set_index(t_stack **a);
 int		stack_size(t_stack *stack);
 void	add_last(t_stack **head, t_stack *node);
 void	free_stack(t_stack *stack);
